3db1fd8d83ad9a9be2b62442fa9bfd8d25cc5e21
gar1k0491 <xgnhr8ebQ>
2021-11-14 00:06:37 +0300

    done lesson3

--- Lesson3/main.go
+++ Lesson3/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("1 + 1 =", 1+1)
+	//Srings
+	fmt.Println("1 + 1 =", 1+1.0)
+	fmt.Println(len("Thirty five symbol length of string"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello " + "World\n")
+	//Boolean types
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+	//Answer1: BIN type
+	//Answer2: 255
+	//Answer3: 1364067664
+	fmt.Println("32132 × 42452 =", 32132*42452)
+	//Answer4: String is a line of symbols. For find length use operation "len"
+	//Answer5: false || false || true = true
+	fmt.Println((true && false) || (false && true) || !(false && false))
+
 }
