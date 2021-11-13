7a68581c7dc6e0d01c69237ae43aeba9b14c2f74
Xmmm <Xmmm@MFNT-001.fnt.corp>
2021-11-13 18:55:57 +0300

    My 1st Commit

---
+++ Lesson1/Hell.o.go
@@ -0,0 +1,16 @@
+//http://golang-book.ru/
+package main
+	 //Ответ на 1ый вопрос. Пробел и таб - разделители
+import "fmt" // Ответ на 3ий вопрос. Файлы в пакете fmt начинаются с package fmt
+
+/* Ответ на 4ый вопрос
+import "os"
+func main() {
+	os.Exit()
+}
+ */
+
+func main() {
+	fmt.Println("Hello, my name is Igor!")
+
+}
