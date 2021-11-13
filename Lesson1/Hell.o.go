de434ff9a9a375925b3db96747ca7fce119e4db7
gar1k0491 <xgnhr8ebQ>
2021-11-13 19:04:50 +0300

    Push from Goland

--- Lesson1/Hell.o.go
+++ Lesson1/Hell.o.go
@@ -1,6 +1,7 @@
 //http://golang-book.ru/
 package main
-	 //Ответ на 1ый вопрос. Пробел и таб - разделители
+
+//Ответ на 1ый вопрос. Пробел и таб - разделители
 import "fmt" // Ответ на 3ий вопрос. Файлы в пакете fmt начинаются с package fmt
 
 /* Ответ на 4ый вопрос
@@ -8,7 +9,7 @@ import "os"
 func main() {
 	os.Exit()
 }
- */
+*/
 
 func main() {
 	fmt.Println("Hello, my name is Igor!")
@@ -16,3 +17,4 @@ func main() {
 }
 
 //Second commit
+//Push from Goland
