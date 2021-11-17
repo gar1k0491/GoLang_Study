c9ac9dd8781cc2a32621179e0f5e9cfa2cdef479
gar1k0491 <xgnhr8ebQ>
2021-11-17 08:21:55 +0300

    Lesson4

--- Lesson4/lesson4.go
+++ Lesson4/lesson4.go
@@ -2,7 +2,43 @@ package main
 
 import "fmt"
 
+var x string = "Hello World MTHFCKR"
+
 func main() {
-	var x string = "Hello World"
 	fmt.Println(x)
+
+	var y string
+	y = "first"
+	fmt.Println(y)
+	y = "second"
+	fmt.Println(y)
+
+	var z string
+	z = "first "
+	fmt.Println(z)
+	z = z + "second "
+	fmt.Println(z)
+	z += "third"
+	fmt.Println(z)
+
+	var q string
+	var w string
+	q = "hello"
+	w = "hello"
+	fmt.Println(q == w)
+
+	var e = 5
+	fmt.Println(e)
+
+	var r = "Hello GO!"
+	fmt.Println(r)
+
+	nameMy := "Igor"
+	fmt.Println("My name is", nameMy)
+
+}
+
+func f() {
+	fmt.Println(x)
+
 }
