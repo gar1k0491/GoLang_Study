571da9a52f8c6dbf30aa6477f65b325fe33b0063
gar1k0491 <xgnhr8ebQ>
2021-11-17 22:55:40 +0300

    Lesson4

--- Lesson4/lesson4.go
+++ Lesson4/lesson4.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var x string = "Hello World MTHFCKR"
 
@@ -36,9 +38,38 @@ func main() {
 	nameMy := "Igor"
 	fmt.Println("My name is", nameMy)
 
-}
+	const t string = "Hello"
+	//t = "Hell"
+	fmt.Println(t)
 
-func f() {
-	fmt.Println(x)
+	fmt.Println("Enter a number (x2): ")
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	output := input * 2
+
+	fmt.Println(output)
+
+	//Answer1: var or :=
+	//Answer2: 6
+	s := 12
+	s += 3
+	fmt.Println(s)
+	//Answer3: View area is {}
+	//Answer4: var editable, const uneditable
+
+	//Answer5:
+	fmt.Println("Input temperature: ")
+	var faren float64
+	fmt.Scanf("%f", &faren)
+	cels := (faren - 32) * 5 / 9
+	fmt.Println(cels)
+
+	//Answer6:
+	fmt.Println("Input distance: ")
+	var feet float64
+	fmt.Scanf("%f", &feet)
+	meters := feet / 3.281
+	fmt.Println(meters)
 
 }
