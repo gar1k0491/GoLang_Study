908a62d8d2140600b6560502c0fe11b694ff5aff
gar1k0491 <xgnhr8ebQ>
2021-11-19 23:42:56 +0300

    Lesson 6 not yet

--- Lesson6/lesson6.go
+++ Lesson6/lesson6.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println()
+	var x [5]int
+	x[4] = 100
+	fmt.Println(x)
+	///////////////////////////////////////////
+	u := [5]float64{
+		98,
+		93,
+		77,
+		82,
+		83,
+	}
+
+	var total float64 = 0
+	for _, v := range u {
+		total += v
+	}
+	fmt.Println(total / float64(len(u)))
+
+	///////////////////////////////////////////
+
 }
