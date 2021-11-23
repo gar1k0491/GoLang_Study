b212d80e97a6102a2f6949d90439dabd39293387
gar1k0491 <xgnhr8ebQ>
2021-11-23 12:47:41 +0300

    Lesson 7 begin

--- Lesson7/lesson7.go
+++ Lesson7/lesson7.go
@@ -1,9 +1,17 @@
+//http://golang-book.ru/
 package main
 
 import "fmt"
 
+func average(ww []float64) float64 {
+	total := 0.0
+	for _, v := range ww {
+		total += v
+	}
+	return total / float64(len(ww))
+}
+
 func main() {
-
-	fmt.Println()
-
+	xs := []float64{98, 93, 77, 82, 83}
+	fmt.Println(average(xs))
 }
