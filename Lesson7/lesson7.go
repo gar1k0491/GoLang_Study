96d81e330fba30351a91ed32d52765f270ccb5e9
gar1k0491 <xgnhr8ebQ>
2021-12-05 19:44:31 +0300

    Lesson7 done

--- Lesson7/lesson7.go
+++ Lesson7/lesson7.go
@@ -12,11 +12,16 @@ func average(xs []float64) float64 {
 }
 
 func main() {
-	xs := []float64{98,93,77,82,83}
+	xs := []float64{98, 93, 77, 82, 83}
 	fmt.Println(average(xs))
 
 	fmt.Println(f1())
-	
+
+	defer func() {
+		str := recover()
+		fmt.Println(str)
+	}()
+	panic("PANIC")
 }
 
 func f1() int {
@@ -26,3 +31,4 @@ func f2() int {
 	return 1
 }
 
+//Answer1: fucn sum(summ []float64) float64
