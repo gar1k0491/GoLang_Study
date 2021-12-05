e1f66ce1baae7a73daaf1a5cbf0373bdd1a3fc71
gar1k0491 <xgnhr8ebQ>
2021-12-05 19:43:57 +0300

    Answer2 not yet

---
+++ Lesson7/answer2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Enter to get half of number:")
+
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	var output float64 = input / 2
+	for output {
+		if output%2 == 0 {
+			fmt.Println(output, "True")
+		} else {
+			fmt.Println(output, "False")
+		}
+
+	}
+
+	fmt.Println(output)
+}
