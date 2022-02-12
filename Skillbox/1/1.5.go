1c7ca223e5026ce87b704177fdabe1455b00ab9d
Xmmm <gar1k0491@gmail.com>
2022-02-12 12:15:21 +0300

    1st Lesson done

---
+++ Skillbox/1/1.5.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 30
+	fmt.Println("Hello, I’m Igor.")
+	fmt.Println("I’m", age, "years old.")
+	fmt.Println("Looking forward to the next lesson!")
+
+}
