9e9928a089aace99eaa59a726ea5b3a24519c3ac
Xmmm <gar1k0491@gmail.com>
2022-02-13 14:20:17 +0300

    2.2 Lesson done

--- Skillbox/2/2.2.go
+++ Skillbox/2/2.2.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var apartment4xRooms int = 98000
-	var shoesW int = 41
+	var apartment4xRoomsPrice int = 98000
+	var shoeSize int = 41
 	var zooCount int = 63
-	var newElectroProduct int = 1200
+	var newElectroProductPrice int = 1200
 
-	fmt.Println("Apartment price -", apartment4xRooms, "$")
-	fmt.Println("Shoe size #368 -", shoesW)
+	fmt.Println("Apartment price -", apartment4xRoomsPrice, "$")
+	fmt.Println("Shoe size #368 -", shoeSize)
 	fmt.Println("Number of animals in the zoo -", zooCount)
-	fmt.Println("The price of a new phone -", newElectroProduct, "$")
+	fmt.Println("The price of a new phone -", newElectroProductPrice, "$")
 }
