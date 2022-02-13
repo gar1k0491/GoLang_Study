196deb726445926cbd99154b816ec826194d0cd0
Xmmm <gar1k0491@gmail.com>
2022-02-13 14:18:29 +0300

    2.2 Lesson done

--- Skillbox/2/2.2.go
+++ Skillbox/2/2.2.go
@@ -6,10 +6,10 @@ func main() {
 	var apartment4xRooms int = 98000
 	var shoesW int = 41
 	var zooCount int = 63
-	var newElectrobProduct int = 1200
+	var newElectroProduct int = 1200
 
 	fmt.Println("Apartment price -", apartment4xRooms, "$")
 	fmt.Println("Shoe size #368 -", shoesW)
 	fmt.Println("Number of animals in the zoo -", zooCount)
-	fmt.Println("The price of a new phone -", newElectrobProduct, "$")
+	fmt.Println("The price of a new phone -", newElectroProduct, "$")
 }
