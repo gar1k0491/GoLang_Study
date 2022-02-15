15cd1f7c545b57d10ca57c170262b22dbeb647e8
gar1k0491 <xgnhr8ebQ>
2022-02-15 10:57:10 +0300

    3.5.3 Task - done

---
+++ Skillbox/3/3.5.3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Обмен местами значений переменных")
+	a := 42
+	b := 153
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
+
+	// обмен местами значений переменных
+
+	fmt.Println("a:", b)
+	fmt.Println("b:", a)
+}
